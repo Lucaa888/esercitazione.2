66c82f476ab130651a2e0956b53ba79921f0ca9a
Lucaa888 <illiano.luca.li@gmail.com>
2024-10-13 17:49:12 +0200

    esercitazione con modifiche

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ function generateRandomInteger(min, max) {
 const numeroCasuale = generateRandomInteger(1, 100);
 let tentativi = 5;
 
+const input = document.querySelector('#userInput');
+const message = document.querySelector('#message');
+const submitBtn = document.querySelector('#submitBtn');
+
+submitBtn.addEventListener('click', gioca);
+
 function gioca() {
-    let input = document.getElementById('userInput').value;
-    let numeroUtente = parseInt(input);
-    let message = document.getElementById('message');
+    const numeroUtente = parseInt(input.value);
 
     if (isNaN(numeroUtente) || numeroUtente < 1 || numeroUtente > 100) {
         message.textContent = "Input non valido. Inserisci un numero tra 1 e 100.";
@@ -17,7 +21,8 @@ function gioca() {
 
     if (numeroUtente === numeroCasuale) {
         message.textContent = "Bravo, hai vinto! Il numero era " + numeroCasuale;
-        document.getElementById('userInput').disabled = true;
+        input.disabled = true;
+        submitBtn.disabled = true;
         return;
     } else if (numeroUtente > numeroCasuale) {
         message.textContent = "Troppo grande.";
@@ -29,7 +34,8 @@ function gioca() {
 
     if (tentativi === 0) {
         message.textContent = "Mi dispiace, hai esaurito i tentativi. Il numero corretto era " + numeroCasuale;
-        document.getElementById('userInput').disabled = true;
+        input.disabled = true;
+        submitBtn.disabled = true;
     } else {
         message.textContent += " Ti rimangono " + tentativi + " tentativi.";
     }
